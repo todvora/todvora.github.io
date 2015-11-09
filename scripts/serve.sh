62a0edea00505fcd720571b8b68fb6300d4a4ffd
Tomas Dvorak <todvora@gmail.com>
2015-11-09 20:51:31 +0100

    layout changes

---
+++ scripts/serve.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Absolute path to this script. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f $0)
+# Absolute path this script is in. /home/user/bin
+SCRIPTPATH=`dirname $SCRIPT`
+
+cd $SCRIPTPATH/..
+bundle exec jekyll serve
